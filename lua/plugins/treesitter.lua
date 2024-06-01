1647fa3b78fc930b6b1d1a357da3ead5ae3f2383
ThienViet <98856248+ThienVie@users.noreply.github.com>
2024-06-01 18:45:39 +0200

    Edit nvim configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
-return	{
+return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
 			local config = require("nvim-treesitter.configs")
-			config. setup ({
-				ensure_installed = {"lua", "python", "vimdoc"},
+			config.setup({
+				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true }
 			})
